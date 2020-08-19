ff3338f4468d09d9bc2fe26f8d4e9b10ab7c80c9
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-08-19 10:58:20 +0300

    implement ProjectService

--- src/main/java/com/vladislav/todoservice/repositories/ProjectRepository.java
+++ src/main/java/com/vladislav/todoservice/repositories/ProjectRepository.java
@@ -1,11 +1,13 @@
 package com.vladislav.todoservice.repositories;
 
-import com.vladislav.todoservice.documents.Project;
+import com.vladislav.todoservice.documents.ProjectDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface ProjectRepository extends MongoRepository<Project, UUID> {
+public interface ProjectRepository extends MongoRepository<ProjectDocument, UUID> {
+    List<ProjectDocument> findAllByUserId(UUID userId);
 }
--- src/main/java/com/vladislav/todoservice/services/ProjectService.java
+++ src/main/java/com/vladislav/todoservice/services/ProjectService.java
@@ -2,7 +2,13 @@ package com.vladislav.todoservice.services;
 
 import com.proto.todo.GetAllUserProjectsRequest;
 import com.proto.todo.GetAllUserProjectsResponse;
+import com.proto.todo.Project;
 import com.proto.todo.ProjectServiceGrpc;
+import com.vladislav.todoservice.documents.ProjectDocument;
+import com.vladislav.todoservice.pojo.User;
+import com.vladislav.todoservice.repositories.ProjectRepository;
+import com.vladislav.todoservice.utils.mappers.PojoMapper;
+import io.grpc.Context;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +19,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ProjectService extends ProjectServiceGrpc.ProjectServiceImplBase {
 
+    private final ProjectRepository projectRepository;
+    private final PojoMapper<ProjectDocument, Project> projectMapper;
+
     @Override
     public void getAllUserProjects(
             GetAllUserProjectsRequest request, StreamObserver<GetAllUserProjectsResponse> responseObserver
     ) {
-        responseObserver.onError(Status.UNIMPLEMENTED.asRuntimeException());
+        final User user = (User) Context.key("user").get();
+        if (request.getUserId().equals(user.getId().toString()) || user.getRoles().contains(User.Role.ADMIN)) {
+            projectRepository.findAllByUserId(user.getId()).stream()
+                    .map(projectMapper::toDto)
+                    .map(project -> GetAllUserProjectsResponse.newBuilder().setProject(project).build())
+                    .forEach(responseObserver::onNext);
+            responseObserver.onCompleted();
+        } else {
+            responseObserver.onError(Status.PERMISSION_DENIED.asRuntimeException());
+        }
     }
 }
