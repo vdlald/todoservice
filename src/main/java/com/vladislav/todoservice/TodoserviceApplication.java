95d028ed7d331f183166e96530b242ec87e5eb1b
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-08-19 09:00:51 +0300

    init

---
+++ src/main/java/com/vladislav/todoservice/TodoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vladislav.todoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vladislav/todoservice/TodoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladislav.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
