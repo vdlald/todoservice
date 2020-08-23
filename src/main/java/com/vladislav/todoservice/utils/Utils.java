ee01856e6b41aaa19d53b061f9407868e40d1b41
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-08-23 20:54:32 +0300

    refactor

--- src/main/java/com/vladislav/todoservice/utils/Utils.java
+++ src/main/java/com/vladislav/todoservice/utils/Utils.java
@@ -3,16 +3,14 @@ package com.vladislav.todoservice.utils;
 import com.vladislav.todoservice.pojo.User;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
 public class Utils {
 
-    public boolean isAdmin(User user) {
+    public static boolean isAdmin(User user) {
         return user.getRoles().contains(User.Role.ADMIN);
     }
 
-    public StatusRuntimeException permissionDeniedException() {
+    public static StatusRuntimeException permissionDeniedException() {
         return Status.PERMISSION_DENIED.asRuntimeException();
     }
 }
