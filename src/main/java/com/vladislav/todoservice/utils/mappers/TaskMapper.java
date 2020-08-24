8f32b3a8ee78792aef591386039e0898ac2fa11c
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-08-24 10:53:23 +0300

    bug fix

--- src/main/java/com/vladislav/todoservice/utils/mappers/TaskMapper.java
+++ src/main/java/com/vladislav/todoservice/utils/mappers/TaskMapper.java
@@ -5,6 +5,7 @@ import com.vladislav.todoservice.documents.TaskDocument;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.UUID;
 
@@ -12,10 +13,17 @@ import java.util.UUID;
 public class TaskMapper implements PojoMapper<TaskDocument, Task> {
     @Override
     public TaskDocument toDocument(Task task) {
-        return new TaskDocument()
-                .setId(UUID.fromString(task.getId()))
-                .setUserId(UUID.fromString(task.getUserId()))
-                .setProjectId(UUID.fromString(task.getProjectId()))
+        final TaskDocument taskDocument = new TaskDocument();
+        if (!task.getId().isEmpty()) {
+            taskDocument.setId(UUID.fromString(task.getId()));
+        }
+        if (!task.getUserId().isEmpty()) {
+            taskDocument.setUserId(UUID.fromString(task.getUserId()));
+        }
+        if (!task.getProjectId().isEmpty()) {
+            taskDocument.setProjectId(UUID.fromString(task.getProjectId()));
+        }
+        return taskDocument
                 .setTitle(task.getTitle())
                 .setContent(task.getContent())
                 .setCompleted(task.getCompleted())
@@ -30,13 +38,29 @@ public class TaskMapper implements PojoMapper<TaskDocument, Task> {
         return Task.newBuilder()
                 .setId(document.getId().toString())
                 .setUserId(document.getUserId().toString())
-                .setProjectId(document.getProjectId().toString())
+                .setProjectId(uuidToString(document.getProjectId()))
                 .setTitle(document.getTitle())
                 .setContent(document.getContent())
                 .setCompleted(document.getCompleted())
-                .setDeadline(document.getDeadline().toEpochSecond(ZoneOffset.UTC))
-                .setCreatedAt(document.getCreatedAt().toEpochSecond(ZoneOffset.UTC))
-                .setCompletedAt(document.getCompletedAt().toEpochSecond(ZoneOffset.UTC))
+                .setDeadline(localDateTimeToEpochSecond(document.getDeadline()))
+                .setCreatedAt(localDateTimeToEpochSecond(document.getCreatedAt()))
+                .setCompletedAt(localDateTimeToEpochSecond(document.getCompletedAt()))
                 .build();
+    }
+
+    private static String uuidToString(UUID uuid) {
+        if (uuid == null) {
+            return "";
+        } else {
+            return uuid.toString();
+        }
+    }
+
+    private static long localDateTimeToEpochSecond(LocalDateTime localDateTime) {
+        if (localDateTime == null) {
+            return 0;
+        } else {
+            return localDateTime.toEpochSecond(ZoneOffset.UTC);
+        }
     }
 }
